910c42fb0e4ac805bca409a0c62fee2a7f7a484c
LaurentArcos <laurent.arcos@gmail.com>
2023-02-06 16:35:08 +0100

    API GoogleMapReact added to Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,27 @@
-function Map() {
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const MAP_KEY = process.env.REACT_APP_MAP_KEY
+
+function Map( {center, zoom} ) {
   return (
-    <div className="map">
-      Hello World
+    <div className="map-container">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: MAP_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+      </GoogleMapReact>
     </div>
   );
+}
+
+Map.defaultProps = {
+  center: {
+    lat: 43.0724,
+    lng: 6.0646,
+  },
+  zoom: 11,
 }
 
 export default Map;
