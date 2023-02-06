84c8bb8cfda57db84eb0a7f7692765d2e98ca708
LaurentArcos <laurent.arcos@gmail.com>
2023-02-06 16:50:44 +0100

    state with lat and lng created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Map from './components/Map';
 import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
 
   const [loading, SetLoading] = useState(true);
+  const [longitude, SetLongitude] = useState(6.0646);
+  const [latitude, SetLatitude] = useState(43.0724);
 
   return (
     <div className="App">
-      {!loading ? <Map /> : <h1>Please wait</h1>}
+      {!loading ? <Map center={{ lat: latitude, lng: longitude }}/> : <h1>Please wait</h1>}
     </div>
   );
 }
